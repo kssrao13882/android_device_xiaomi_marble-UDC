33be566632803635091e8f360fbe3a67fb109305
kssrao13882 <kssrao13882@gmail.com>
2023-11-16 16:45:38 +0530

    marble: Welcome Octavi Official

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_marble.mk
+    $(LOCAL_DIR)/octavi_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_marble-eng \
-    superior_marble-userdebug \
-    superior_marble-user
+    octavi_marble-eng \
+    octavi_marble-userdebug \
+    octavi_marble-user
--- octavi_marble.mk
+++ octavi_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+# Inherit some common OctaviOS stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 BUILD_WITH_GAPPS := true
 
 # Inherit from marble device.
@@ -19,7 +19,17 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := superior_marble
+PRODUCT_NAME := octavi_marble
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
+
+# Official
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_MAINTAINER := KSSRAO
+
+# Gapps Build
+WITH_GAPPS := true
